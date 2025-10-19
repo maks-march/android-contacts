caaeb6a442c90d192efd8e35f1f951013e5fd95f
marchenko <1maks_2055@mail.ru>
2025-10-19 23:05:22 +0500

    unused imports deleted

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
@@ -1,24 +1,12 @@
 package ru.yandex.practicum.contacts.presentation.messengers
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
