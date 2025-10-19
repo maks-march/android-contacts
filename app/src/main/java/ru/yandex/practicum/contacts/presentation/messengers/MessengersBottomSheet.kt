e9a90e1087528db75827ef08552e0e2fb1c05b82
marchenko <1maks_2055@mail.ru>
2025-10-19 22:55:08 +0500

    MessengersBottomSheet refactored

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/messengers/MessengersBottomSheet.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.yandex.practicum.contacts.R
 import ru.yandex.practicum.contacts.data.models.MessagingApp
+import ru.yandex.practicum.contacts.presentation.ui.components.CommonBottomSheet
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,71 +36,21 @@ fun MessengersBottomSheet(
     onAppsSelected: (Set<MessagingApp>) -> Unit,
     onDismiss: () -> Unit
 ) {
-    ModalBottomSheet(
-        onDismissRequest = onDismiss,
-        dragHandle = { BottomSheetDefaults.DragHandle() }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = stringResource(R.string.filter_by_messaging_app),
-                    style = MaterialTheme.typography.titleLarge
-                )
-                IconButton(onClick = onDismiss) {
-                    Icon(
-                        imageVector = Icons.Default.Close,
-                        contentDescription = stringResource(R.string.close)
-                    )
-                }
-            }
-
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            ) {
-                items(MessagingApp.entries) { app ->
-                    val isSelected = selectedApps.contains(app)
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp),
-                        onClick = {
-                            val newSelection = selectedApps.toMutableSet()
-                            if (isSelected) {
-                                newSelection.remove(app)
-                            } else {
-                                newSelection.add(app)
-                            }
-                            onAppsSelected(newSelection)
-                        },
-                        color = if (isSelected) {
-                            MaterialTheme.colorScheme.primaryContainer
-                        } else {
-                            MaterialTheme.colorScheme.surface
-                        }
-                    ) {
-                        MessengerOption(
-                            isSelected = isSelected,
-                            app = app,
-                            selectedApps = selectedApps,
-                            onAppsSelected = onAppsSelected
-                        )
-                    }
-                }
-            }
+    CommonBottomSheet(
+        title = stringResource(R.string.filter_by_messaging_app),
+        items = MessagingApp.entries,
+        selectedItems = selectedApps,
+        onItemsSelected = onAppsSelected,
+        onDismiss = onDismiss,
+        itemContent = { app, isSelected ->
+            MessengerOption(
+                isSelected = isSelected,
+                app = app,
+                selectedApps = selectedApps,
+                onAppsSelected = onAppsSelected
+            )
         }
-    }
+    )
 }
 
 @Composable
